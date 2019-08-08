e19ed5fe7df1ddb9971ad81ed300f2c075779a35
NKwatra <kwatranishkarsh@gmail.com>
2019-08-08 10:13:56 +0530

    Add signup functionality

--- onlinebooks/authentication/views.py
+++ onlinebooks/authentication/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
 
 
 def sign_up(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.customer.address = form.cleaned_data.get('address')
+            user.customer.mobile_no = form.cleaned_data.get('mobile_no').replace(' ', '')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('authenticate:sign_up')
+    else:        
+        form = SignUpForm()
     return render(request, 'authenticate/sign_up.html', {'form' : form})
--- onlinebooks/user/models.py
+++ onlinebooks/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 app_name = 'Customer'
 
@@ -10,8 +11,20 @@ app_name = 'Customer'
 #   address: the address of user where products are to be delivered
 #   mobile_nos: mobile number of the user
 class Customer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to="images/")
-    address = ArrayField(models.TextField())
-    mobile_no = models.CharField(max_length=15)
+    address = models.TextField()
+    mobile_no = models.CharField(max_length=15, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.customer.save()
 
