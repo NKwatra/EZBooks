42ef1b975f2736740de70bec80f22e173905da15
NKwatra <kwatranishkarsh@gmail.com>
2019-08-08 14:09:31 +0530

    Code refactor: add signin and signup in same template

--- onlinebooks/authentication/urls.py
+++ onlinebooks/authentication/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'authenticate'
 
 urlpatterns = [
+    path('', views.auth, name='authenticate'),
     path('signup', views.sign_up, name='sign_up'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
--- onlinebooks/authentication/views.py
+++ onlinebooks/authentication/views.py
@@ -2,37 +2,35 @@ from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth import authenticate, login as sign_in, logout as sign_out
 
+def auth(request):
+    form = SignUpForm()
+    return render(request, 'authenticate/auth.html', {'form': form})
 
 def sign_up(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            user.customer.address = form.cleaned_data.get('address')
-            user.customer.mobile_no = form.cleaned_data.get('mobile_no').replace(' ', '')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            sign_in(request, user)
-            return redirect('authenticate:sign_up')
-    else:        
-        form = SignUpForm()
-    return render(request, 'authenticate/sign_up.html', {'form' : form})
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        user.refresh_from_db()
+        user.customer.address = form.cleaned_data.get('address')
+        user.customer.mobile_no = form.cleaned_data.get('mobile_no').replace(' ', '')
+        user.save()
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=user.username, password=raw_password)
+        sign_in(request, user)
+        return redirect('authenticate:authenticate')
+    return render(request, 'authenticate/auth.html', {'form': form})   
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            sign_in(request, user)
-            return redirect('authenticate:login')
-        else :
-            error_message = 'Incorrect username or password'
-            return render(request, 'authenticate/login.html', {'error': error_message })
-    return render(request, 'authenticate/login.html') 
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        sign_in(request, user)
+        return redirect('authenticate:authenticate')
+    else :
+        error_message = 'Incorrect username or password'
+        return render(request, 'authenticate/auth.html', {'error': error_message, 'form': SignUpForm() })
 
 def logout(request):
     sign_out(request)
-    return redirect('authenticate:login')
+    return redirect('authenticate:authenticate')
