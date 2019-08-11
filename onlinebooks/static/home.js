700a42a57d7cd3edc4c9171b17b7580b3a067fe5
NKwatra <kwatranishkarsh@gmail.com>
2019-08-11 08:15:50 +0530

    Show book title on hover on large screen devices

--- onlinebooks/static/home.js
+++ onlinebooks/static/home.js
@@ -1,44 +1,34 @@
 $(document).ready(
-    function()
-    {
-        // var move = 0;
-        // var last = null;
+    function () {
         $('.scroll-background').click(
-            function()
-        {
-            // if(last != null &&  !last.is($(this)))
-            //     move = 0;
-           // move += $(this).parent().width();
-            $(this).parent().animate({
-                scrollLeft: $(this).parent().scrollLeft() + $(this).parent().width()
-            }, "slow", function(){
-                $(this).siblings('.scroll-background-left').addClass('d-md-block');
-                if(Math.abs($(this).get(0).scrollWidth - $(this).scrollLeft() - $(this).outerWidth()) <= 10)
-                {
-                    $(this).children('.scroll-background').removeClass('d-md-block');
-                } 
-            });
-           // last = $(this);       
-                
-        });
-        $('.scroll-background-left').click(
-            function()
-        {
-            // if(last.parent().siblings('.scroll-background-left').is($(this)))
-            //     move -= last.parent().width();
-            // else
-            //     move = $(this).siblings('.category-books-container').scrollLeft() - $(this).siblings('.category-books-container').width();
+            function () {
+                $(this).parent().animate({
+                    scrollLeft: $(this).parent().scrollLeft() + $(this).parent().width()
+                }, "slow", function () {
+                    $(this).siblings('.scroll-background-left').addClass('d-md-block');
+                    if (Math.abs($(this).get(0).scrollWidth - $(this).scrollLeft() - $(this).outerWidth()) <= 10) {
+                        $(this).children('.scroll-background').removeClass('d-md-block');
+                    }
+                });
 
-            // if (move < 0)
-            //     move = 0;    
-            var container = $(this).siblings('.category-books-container');  
-            container.animate({
-                scrollLeft : container.scrollLeft() - container.width()
-            }, "slow", function(){
-                $(this).children('.scroll-background').addClass('d-md-block')
-                if($(this).scrollLeft() == 0)
-                    $(this).siblings('.scroll-background-left').removeClass('d-md-block')
-            });    
-        });
+            });
+        $('.scroll-background-left').click(
+            function () {
+                var container = $(this).siblings('.category-books-container');
+                container.animate({
+                    scrollLeft: container.scrollLeft() - container.width()
+                }, "slow", function () {
+                    $(this).children('.scroll-background').addClass('d-md-block')
+                    if ($(this).scrollLeft() == 0)
+                        $(this).siblings('.scroll-background-left').removeClass('d-md-block')
+                });
+            });
+        $('.book-tile').hover(
+            function() {
+                $(this).children('.hidden-title').addClass('d-lg-block');
+            }, function(){
+                $(this).children('.hidden-title').removeClass('d-lg-block');
+            }
+        );    
     }
 )
