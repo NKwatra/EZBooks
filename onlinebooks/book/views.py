252af3734faa9740d96e4ef03daa440c781989ff
NKwatra <kwatranishkarsh@gmail.com>
2019-08-11 12:51:40 +0530

    Create category detail page

--- onlinebooks/book/views.py
+++ onlinebooks/book/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def category(request, category):
+    all_books = Book.objects.filter(category__iexact=category).order_by('avg_rating').reverse()
+    return render(request, 'book/category.html', {'books_list': all_books, 'category': category})
--- onlinebooks/onlinebooks/urls.py
+++ onlinebooks/onlinebooks/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path('', views.home, name='home'),
+    path('books/', include('book.urls')),
     path('admin/', admin.site.urls),
     path('authenticate/', include('authentication.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
